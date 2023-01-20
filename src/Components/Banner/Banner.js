efd1fa6a75ee59fcd8d1a7cda9a2eaa14b1f640f
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-01-21 01:18:12 +0600

    created responsive banner section

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,10 +6,10 @@ const Banner = () => {
             <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/XVJ4HRJ/banner-1.jpg")` }}>
                 <div className='hero-overlay bg-opacity-5'></div>
                 <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-screen-xl">
-                        <h1 className="mb-5 text-3xl">Flower & Gift</h1>
-                        <h2 className="mb-5 text-xl md:text-5xl lg:text-7xl font-semibold">Perfect Bunch <br></br> For Every Occasion</h2>
-                        <button className="btn btn-secondary px-10 rounded-full mt-10">Shop Now</button>
+                    <div className="max-w-screen-xl text-white">
+                        <h1 className="mb-2 md:mb-3 lg:mb-5 text-2xl md:text-3xl">Flower & Gift</h1>
+                        <h2 className="mb-5 text-3xl md:text-5xl lg:text-7xl font-semibold">Perfect Bunch <br></br> For Every Occasion</h2>
+                        <button className="btn btn-secondary px-10 rounded-full  md:mt-7 lg:mt-10">Shop Now</button>
                     </div>
                 </div>
             </div>
