b129cff795b68c15a2bb3a62b9170ef021429f0c
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-01-23 22:35:40 +0600

    added divider section on home page

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -68,7 +68,11 @@ const Products = () => {
     }
     return (
         <div>
-            <div className='mx-5 md:mx-10 lg:mx-20 my-20 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10'>
+            <h1 className='mt-20 md:text-3xl lg:text-5xl font-bold text-center '>New arrival items</h1>
+            <div className='flex items-center justify-center'>
+                <div className="divider w-1/12 mb-5 divide-red-400 text-center"></div>
+            </div>
+            <div className='mx-5 md:mx-10 lg:mx-20 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10'>
                 {
                     products?.map(product => <Product
                         product={product}
