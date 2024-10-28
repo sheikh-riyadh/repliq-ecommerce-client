1eb788d706f463c8322a360614d1cc2d81333642
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2024-10-28 19:10:57 +0600

    fixed banner section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { router } from './Routes/Routes';
 import 'animate.css';
 import { Toaster } from 'react-hot-toast';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 function App() {
   return (
     <div className='font-montserrat'>
-      <RouterProvider router={router}>
-      </RouterProvider>
+      <ParallaxProvider>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </ParallaxProvider>
       <Toaster></Toaster>
     </div>
   );
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
+import banner from "../../Assets/banner-1.jpg";
 
 const Banner = () => {
-    return (
-        <div>
-            <div className="hero min-h-screen bg-banner bg-cover">
-                <div className='hero-overlay bg-opacity-5'></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-screen-xl text-white">
-                        <h1 className="mb-2 md:mb-3 lg:mb-5 text-2xl md:text-3xl">Flower & Gift</h1>
-                        <h2 className="mb-5 text-3xl md:text-5xl lg:text-7xl font-semibold">Perfect Bunch <br></br> For Every Occasion</h2>
-                        <button className="btn btn-secondary px-10 rounded-full  md:mt-7 lg:mt-10">Shop Now</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="relative">
+      <div className="hero h-96 md:h-[450px] lg:h-[600px] xl:min-h-screen w-full z-10 relative">
+        <div className="hero-overlay bg-opacity-5"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-screen-xl text-white">
+            <h1 className="mb-2 md:mb-3 lg:mb-5 text-2xl md:text-3xl">
+              Flower & Gift
+            </h1>
+            <h2 className="mb-5 text-3xl md:text-5xl lg:text-7xl font-semibold">
+              Perfect Bunch <br></br> For Every Occasion
+            </h2>
+            <button className="btn btn-secondary px-10 rounded-full  md:mt-7 lg:mt-10">
+              Shop Now
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="absolute w-full top-0">
+        <img className="h-96 md:h-[450px] lg:h-[600px] xl:min-h-screen w-full" src={banner} alt="banner" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
